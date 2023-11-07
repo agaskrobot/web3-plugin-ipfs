d6ccccb35382bf7fedbf6c5cd405166bee9ff439
Aga <agnieszka.skrobot1@gmail.com>
2023-11-07 17:31:41 +0100

    Remove unnecesary files

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,12 +14,22 @@
 
 const webpackPreprocessor = require('@cypress/webpack-preprocessor');
 const webpackOptions = require('../webpack.config.js');
+const fs = require('fs')
 
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
+	on('task', {
+		readFileMaybe(filename) {
+			if (fs.existsSync(filename)) {
+				return fs.readFileSync(filename);
+			}
+
+			return null;
+		},
+	});
 	on('file:preprocessor', webpackPreprocessor({ webpackOptions }));
 	return config;
 };
